73375d58af54a2a93c9713b2c30c6799b5de45ab
Azizsige <kurniawan00azizsige@gmail.com>
2020-11-18 10:53:39 +0700

    17/11/2020 - Jumbotron

--- code/js/app.js
+++ code/js/app.js
@@ -1,14 +1,21 @@
 const checkbox = document.getElementById('checkbox');
 const span = document.querySelectorAll('#span-hamburger');
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.navbar-menu');
 
 checkbox.addEventListener('click', () => {
   span.forEach(spans => {
     if(checkbox.checked){
     spans.classList.toggle('active');
     spans.classList.remove('close');
+    menu.classList.toggle('open');
+    menu.classList.remove('close')
+    
   } else {
     spans.classList.toggle('close');
     spans.classList.toggle('active');
+    menu.classList.toggle('open');
+    menu.classList.toggle('close');
   }
   });
 })
